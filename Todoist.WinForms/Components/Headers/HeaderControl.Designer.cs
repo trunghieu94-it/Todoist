a8a49e2bfdfa080abb06623715b049f068baf101
hieubstu123-lab <hieubstu123@gmail.com>
2026-04-04 19:53:00 +0700

    feat: add a new todoitem

--- Todoist.WinForms/Components/Headers/HeaderControl.Designer.cs
+++ Todoist.WinForms/Components/Headers/HeaderControl.Designer.cs
@@ -32,9 +32,6 @@ namespace Todoist.WinForms.Views.Components
             this.lblTitle = new Todoist.WinForms.Views.Components.IconLabel();
             this.btnSort = new Todoist.WinForms.Views.Components.CustomButton();
             this.lblDate = new Todoist.WinForms.Views.Components.IconLabel();
-            this.btnGroup = new Todoist.WinForms.Views.Components.CustomButton();
-            this.btnGrid = new Todoist.WinForms.Views.Components.CustomButton();
-            this.btnList = new Todoist.WinForms.Views.Components.CustomButton();
             this.pnlHeader.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,9 +45,6 @@ namespace Todoist.WinForms.Views.Components
             this.pnlHeader.Controls.Add(this.lblTitle);
             this.pnlHeader.Controls.Add(this.btnSort);
             this.pnlHeader.Controls.Add(this.lblDate);
-            this.pnlHeader.Controls.Add(this.btnGroup);
-            this.pnlHeader.Controls.Add(this.btnGrid);
-            this.pnlHeader.Controls.Add(this.btnList);
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
             this.pnlHeader.Name = "pnlHeader";
             this.pnlHeader.Size = new System.Drawing.Size(744, 75);
@@ -89,7 +83,7 @@ namespace Todoist.WinForms.Views.Components
             this.btnSort.IconPadding = 10;
             this.btnSort.IconSize = new System.Drawing.Size(24, 24);
             this.btnSort.IconTextSpacing = 30;
-            this.btnSort.Location = new System.Drawing.Point(518, 20);
+            this.btnSort.Location = new System.Drawing.Point(316, 20);
             this.btnSort.Name = "btnSort";
             this.btnSort.Size = new System.Drawing.Size(85, 40);
             this.btnSort.TabIndex = 5;
@@ -112,81 +106,6 @@ namespace Todoist.WinForms.Views.Components
             this.lblDate.Text = "Sunday, January 11";
             this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // btnGroup
-            // 
-            this.btnGroup.BackColor = System.Drawing.SystemColors.Control;
-            this.btnGroup.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.btnGroup.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btnGroup.BorderRadius = 10;
-            this.btnGroup.BorderSize = 0;
-            this.btnGroup.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnGroup.FlatAppearance.BorderSize = 0;
-            this.btnGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGroup.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGroup.ForeColor = System.Drawing.Color.Black;
-            this.btnGroup.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
-            this.btnGroup.Icon = global::Todoist.WinForms.Properties.Resources.group;
-            this.btnGroup.IconPadding = 6;
-            this.btnGroup.IconSize = new System.Drawing.Size(24, 24);
-            this.btnGroup.IconTextSpacing = 30;
-            this.btnGroup.Location = new System.Drawing.Point(609, 20);
-            this.btnGroup.Name = "btnGroup";
-            this.btnGroup.Size = new System.Drawing.Size(85, 40);
-            this.btnGroup.TabIndex = 6;
-            this.btnGroup.Text = "Group";
-            this.btnGroup.TextColor = System.Drawing.Color.Black;
-            this.btnGroup.UseVisualStyleBackColor = false;
-            // 
-            // btnGrid
-            // 
-            this.btnGrid.BackColor = System.Drawing.SystemColors.Control;
-            this.btnGrid.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.btnGrid.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btnGrid.BorderRadius = 10;
-            this.btnGrid.BorderSize = 0;
-            this.btnGrid.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnGrid.FlatAppearance.BorderSize = 0;
-            this.btnGrid.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGrid.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGrid.ForeColor = System.Drawing.Color.Black;
-            this.btnGrid.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
-            this.btnGrid.Icon = global::Todoist.WinForms.Properties.Resources.grid;
-            this.btnGrid.IconPadding = 10;
-            this.btnGrid.IconSize = new System.Drawing.Size(24, 24);
-            this.btnGrid.IconTextSpacing = 30;
-            this.btnGrid.Location = new System.Drawing.Point(336, 20);
-            this.btnGrid.Name = "btnGrid";
-            this.btnGrid.Size = new System.Drawing.Size(85, 40);
-            this.btnGrid.TabIndex = 8;
-            this.btnGrid.Text = "Grid";
-            this.btnGrid.TextColor = System.Drawing.Color.Black;
-            this.btnGrid.UseVisualStyleBackColor = false;
-            // 
-            // btnList
-            // 
-            this.btnList.BackColor = System.Drawing.SystemColors.Control;
-            this.btnList.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.btnList.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btnList.BorderRadius = 10;
-            this.btnList.BorderSize = 0;
-            this.btnList.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnList.FlatAppearance.BorderSize = 0;
-            this.btnList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnList.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnList.ForeColor = System.Drawing.Color.Black;
-            this.btnList.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
-            this.btnList.Icon = global::Todoist.WinForms.Properties.Resources.list;
-            this.btnList.IconPadding = 10;
-            this.btnList.IconSize = new System.Drawing.Size(24, 24);
-            this.btnList.IconTextSpacing = 30;
-            this.btnList.Location = new System.Drawing.Point(427, 20);
-            this.btnList.Name = "btnList";
-            this.btnList.Size = new System.Drawing.Size(85, 40);
-            this.btnList.TabIndex = 7;
-            this.btnList.Text = "List";
-            this.btnList.TextColor = System.Drawing.Color.Black;
-            this.btnList.UseVisualStyleBackColor = false;
-            // 
             // HeaderControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -203,9 +122,6 @@ namespace Todoist.WinForms.Views.Components
         #endregion
 
         private CustomButton btnSort;
-        private CustomButton btnGroup;
-        private CustomButton btnList;
-        private CustomButton btnGrid;
         private IconLabel lblDate;
         private IconLabel lblTitle;
         private BorderPanel pnlHeader;
